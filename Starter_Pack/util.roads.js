81ff8601b4ac6a43b6a78d2bb3bafa6da0e0ef5b
Elwin <abc602456615@163.com>
2020-07-21 12:39:42 +0100

    update roads

--- Starter_Pack/util.roads.js
+++ Starter_Pack/util.roads.js
@@ -3,7 +3,9 @@ var utilRoads = {
         var sources = spawn.room.find(FIND_SOURCES);
         for (var j = 0; j < sources.length; j++)
         {
-            var chemin = spawn.pos.findPathTo(sources[j].pos);
+            var chemin = spawn.room.findPath(spawn.pos, sources[j].pos, {
+                ignoreCreeps: true, ignoreRoads: true
+            });
             for (var i = 0; i < chemin.length; i++)
             {
                 spawn.room.createConstructionSite(chemin[i].x,chemin[i].y, STRUCTURE_ROAD);
