ab78895b1e669b9d3593da711b68367da31df00e
Elwin <abc602456615@163.com>
2020-07-21 11:44:53 +0100

    bug fix

--- Starter_Pack/util.roads.js
+++ Starter_Pack/util.roads.js
@@ -1,12 +1,12 @@
 var utilRoads = {
     run: function (spawn) {
-        var sources = Game.spawns[spawn].room.find(FIND_SOURCES);
+        var sources = spawn.room.find(FIND_SOURCES);
         for (var j = 0; j < sources.length; j++)
         {
-            var chemin = Game.spawns[spawn].pos.findPathTo(sources[j].pos);
+            var chemin = spawn.pos.findPathTo(sources[j].pos);
             for (var i = 0; i < chemin.length; i++)
             {
-                Game.spawns[spawn].room.createConstructionSite(chemin[i].x,chemin[i].y, STRUCTURE_ROAD);
+                spawn.room.createConstructionSite(chemin[i].x,chemin[i].y, STRUCTURE_ROAD);
             }
         }
     }
