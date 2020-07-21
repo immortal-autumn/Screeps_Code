eebcd93d00e321372fe8e1572e1e11fea8f924db
Elwin <abc602456615@163.com>
2020-07-21 11:39:50 +0100

    update Roads

--- Starter_Pack/util.roads.js
+++ Starter_Pack/util.roads.js
@@ -1,6 +1,14 @@
 var utilRoads = {
-    run: function (creep) {
-        creep.pos.createConstructionSite(STRUCTURE_ROAD);
+    run: function (spawn) {
+        var sources = Game.spawns[spawn].room.find(FIND_SOURCES);
+        for (var j = 0; j < sources.length; j++)
+        {
+            var chemin = Game.spawns[spawn].pos.findPathTo(sources[j].pos);
+            for (var i = 0; i < chemin.length; i++)
+            {
+                Game.spawns[spawn].room.createConstructionSite(chemin[i].x,chemin[i].y, STRUCTURE_ROAD);
+            }
+        }
     }
 };
 
