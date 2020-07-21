c95293c83ce188116b8fe13bd427f583fe16c65f
Elwin <abc602456615@163.com>
2020-07-21 18:50:35 +0100

    update

--- Starter_Pack/util.roads.js
+++ Starter_Pack/util.roads.js
@@ -1,16 +1,18 @@
 var utilRoads = {
     run: function (spawn, rooms) {
         rooms.forEach(room => {
-            var sources = Game.rooms[room].find(FIND_SOURCES);
-            if (sources) {
-                for (var j = 0; j < sources.length; j++)
-                {
-                    var chemin = spawn.room.findPath(spawn.pos, sources[j].pos, {
-                        ignoreCreeps: true, ignoreRoads: true
-                    });
-                    for (var i = 0; i < chemin.length; i++)
+            if(Game.rooms[room]) {
+                var sources = Game.rooms[room].find(FIND_SOURCES);
+                if (sources) {
+                    for (var j = 0; j < sources.length; j++)
                     {
-                        spawn.room.createConstructionSite(chemin[i].x,chemin[i].y, STRUCTURE_ROAD);
+                        var chemin = spawn.room.findPath(spawn.pos, sources[j].pos, {
+                            ignoreCreeps: true, ignoreRoads: true
+                        });
+                        for (var i = 0; i < chemin.length; i++)
+                        {
+                            spawn.room.createConstructionSite(chemin[i].x,chemin[i].y, STRUCTURE_ROAD);
+                        }
                     }
                 }
             }
