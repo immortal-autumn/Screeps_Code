0f1f15e8bc8ac787dfd6197eb723da15ae091e32
Elwin <abc602456615@163.com>
2020-07-23 19:08:35 +0100

    Repair claim

--- Starter_Pack/init.claim.js
+++ Starter_Pack/init.claim.js
@@ -9,9 +9,11 @@ var initClaim = {
         }
         let controller = creep.room.controller;
         if (controller && !controller.my) {
-            if (creep.attackController(controller[0]) === ERR_NOT_IN_RANGE || creep.claimController(controller[0]) === ERR_NOT_IN_RANGE
-                || creep.reserveController(controller[0]) === ERR_NOT_IN_RANGE) {
-                creep.moveTo(controller[0], {visualizePathStyle: {stroke: '#5b3475'}});
+            if (creep.attack(controller) === ERR_NOT_IN_RANGE
+                || creep.attackController(controller) === ERR_NOT_IN_RANGE
+                || creep.claimController(controller) === ERR_NOT_IN_RANGE
+                || creep.reserveController(controller) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(controller, {visualizePathStyle: {stroke: '#5b3475'}});
             }
         }
     }
