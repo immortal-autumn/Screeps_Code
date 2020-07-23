4408e229d067306bdacc2cee73083301d0cd0ac0
Elwin <abc602456615@163.com>
2020-07-23 15:11:57 +0100

    Update construction

--- Starter_Pack/init.cons.js
+++ Starter_Pack/init.cons.js
@@ -3,15 +3,14 @@ var uRoad = require('util.roads');
 // Spawning creeps based on probability and spawning roads
 
 const nameBefore = 'CN';
-var nextCreep = 0;
 
 var initCons = {
     run: function (rooms) {
         for (let i in Game.spawns) {
             let spawnInfo = Game.spawns[i];
             uRoad.run(spawnInfo, rooms);
-
-            let randCase = Math.floor(Math.random() * 10);
+            let nextCreep = Math.floor(Math.random() * 10000);
+            let randCase = Math.floor(Math.random() * 100);
             let sucSpa;
             let amount = spawnInfo.room.find(FIND_MY_CREEPS).length;
             if (amount >= 10 && spawnInfo.room.find(FIND_MY_CREEPS, {
@@ -21,11 +20,6 @@ var initCons = {
             }).length === 0) {
                 sucSpa = spawnInfo.spawnCreep([CARRY, CARRY, MOVE, MOVE], nameBefore + nextCreep,
                     {memory: {'belongs': 0, 'role': 'carry'}});
-                if (sucSpa === 0) {
-                    nextCreep++;
-                } else if (sucSpa === -3) {
-                    nextCreep++;
-                }
                 return;
             }
             if (amount >= 13 && spawnInfo.room.find(FIND_MY_CREEPS, {
@@ -35,19 +29,14 @@ var initCons = {
             }).length === 0) {
                 sucSpa = spawnInfo.spawnCreep([CLAIM, MOVE], nameBefore + nextCreep,
                     {memory: {'belongs': 0, 'role': 'claim'}});
-                if (sucSpa === 0) {
-                    nextCreep++;
-                } else if (sucSpa === -3) {
-                    nextCreep++;
-                }
                 return;
             }
             if (amount === 0) {
                 sucSpa = spawnInfo.spawnCreep([WORK, WORK, CARRY, MOVE], nameBefore + nextCreep,
                     {memory: {'belongs': 0, 'role': 'harvest'}});
-            } else if (amount >= 8 && amount % 2 === 0) {
+            } else if (amount >= 8 && randCase % 2 === 0) {
                 // 470 energy.
-                if (randCase < 0.5) {
+                if (randCase < 50) {
                     sucSpa = spawnInfo.spawnCreep([ATTACK, ATTACK, ATTACK, MOVE, TOUGH, TOUGH, TOUGH, MOVE, MOVE, MOVE], nameBefore + nextCreep,
                         {memory: {'belongs': 0, 'role': 'weapon'}});
                 } else {
@@ -55,18 +44,13 @@ var initCons = {
                         {memory: {'belongs': 0, 'role': 'weapon'}});
                 }
             } else {
-                if (amount <= 10 && randCase <= 0.01) {
+                if (amount <= 10) {
                     sucSpa = spawnInfo.spawnCreep([WORK, CARRY, MOVE], nameBefore + nextCreep);
-                } else if (amount <= 20 && randCase > 0.01 && randCase < 0.2) {
+                } else if (amount <= 20) {
                     sucSpa = spawnInfo.spawnCreep([WORK, WORK, CARRY, MOVE], nameBefore + nextCreep);
                 } else {
                     sucSpa = spawnInfo.spawnCreep([WORK, WORK, WORK, CARRY, MOVE, MOVE], nameBefore + nextCreep);
                 }
-            }
-            if (sucSpa === 0) {
-                nextCreep++;
-            } else if (sucSpa === -3) {
-                nextCreep++;
             }
         }
     }
