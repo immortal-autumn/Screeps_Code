932ac47290a508fe5d91979355dcf823c7bd7a25
Elwin <abc602456615@163.com>
2020-07-23 15:20:57 +0100

    UDATE CONSTRUCTION

--- Starter_Pack/init.cons.js
+++ Starter_Pack/init.cons.js
@@ -9,7 +9,9 @@ var initCons = {
         for (let i in Game.spawns) {
             let spawnInfo = Game.spawns[i];
             uRoad.run(spawnInfo, rooms);
-            let nextCreep = Math.floor(Math.random() * 10000);
+            if (typeof spawnInfo.memory['nextCreep'] == 'undefined') {
+                spawnInfo.memory.nextCreep = Math.floor(Math.random() * 10000);
+            }
             let randCase = Math.floor(Math.random() * 100);
             let sucSpa;
             let amount = spawnInfo.room.find(FIND_MY_CREEPS).length;
@@ -18,39 +20,47 @@ var initCons = {
                     return creep.memory.role === 'carry';
                 }
             }).length === 0) {
-                sucSpa = spawnInfo.spawnCreep([CARRY, CARRY, MOVE, MOVE], nameBefore + nextCreep,
+                sucSpa = spawnInfo.spawnCreep([CARRY, CARRY, MOVE, MOVE],
+                    nameBefore + spawnInfo.memory.nextCreep,
                     {memory: {'belongs': 0, 'role': 'carry'}});
-                return;
-            }
-            if (amount >= 13 && spawnInfo.room.find(FIND_MY_CREEPS, {
+            } else if (amount >= 13 && spawnInfo.room.find(FIND_MY_CREEPS,
+                {
                 filter: function (creep) {
                     return creep.memory.role === 'claim';
                 }
             }).length === 0) {
-                sucSpa = spawnInfo.spawnCreep([CLAIM, MOVE], nameBefore + nextCreep,
+                sucSpa = spawnInfo.spawnCreep([CLAIM, MOVE],
+                    nameBefore + spawnInfo.memory.nextCreep,
                     {memory: {'belongs': 0, 'role': 'claim'}});
-                return;
-            }
-            if (amount === 0) {
-                sucSpa = spawnInfo.spawnCreep([WORK, WORK, CARRY, MOVE], nameBefore + nextCreep,
+            } else if (amount === 0) {
+                sucSpa = spawnInfo.spawnCreep([WORK, WORK, CARRY, MOVE],
+                    nameBefore + spawnInfo.memory.nextCreep,
                     {memory: {'belongs': 0, 'role': 'harvest'}});
             } else if (amount >= 8 && randCase % 2 === 0) {
                 // 470 energy.
                 if (randCase < 50) {
-                    sucSpa = spawnInfo.spawnCreep([ATTACK, ATTACK, ATTACK, MOVE, TOUGH, TOUGH, TOUGH, MOVE, MOVE, MOVE], nameBefore + nextCreep,
+                    sucSpa = spawnInfo.spawnCreep([ATTACK, ATTACK, ATTACK, MOVE, TOUGH, TOUGH, TOUGH, MOVE, MOVE, MOVE],
+                        nameBefore + spawnInfo.memory.nextCreep,
                         {memory: {'belongs': 0, 'role': 'weapon'}});
                 } else {
-                    sucSpa = spawnInfo.spawnCreep([RANGED_ATTACK, RANGED_ATTACK, TOUGH, TOUGH, MOVE, MOVE, MOVE], nameBefore + nextCreep,
+                    sucSpa = spawnInfo.spawnCreep([RANGED_ATTACK, RANGED_ATTACK, TOUGH, TOUGH, MOVE, MOVE, MOVE],
+                        nameBefore + spawnInfo.memory.nextCreep,
                         {memory: {'belongs': 0, 'role': 'weapon'}});
                 }
             } else {
                 if (amount <= 10) {
-                    sucSpa = spawnInfo.spawnCreep([WORK, CARRY, MOVE], nameBefore + nextCreep);
+                    sucSpa = spawnInfo.spawnCreep([WORK, CARRY, MOVE],
+                        nameBefore + spawnInfo.memory.nextCreep);
                 } else if (amount <= 20) {
-                    sucSpa = spawnInfo.spawnCreep([WORK, WORK, CARRY, MOVE], nameBefore + nextCreep);
+                    sucSpa = spawnInfo.spawnCreep([WORK, WORK, CARRY, MOVE],
+                        nameBefore + spawnInfo.memory.nextCreep);
                 } else {
-                    sucSpa = spawnInfo.spawnCreep([WORK, WORK, WORK, CARRY, MOVE, MOVE], nameBefore + nextCreep);
+                    sucSpa = spawnInfo.spawnCreep([WORK, WORK, WORK, CARRY, MOVE, MOVE],
+                        nameBefore + spawnInfo.memory.nextCreep);
                 }
+            }
+            if (sucSpa === OK || sucSpa === ERR_NAME_EXISTS) {
+                delete spawnInfo.memory.nextCreep;
             }
         }
     }
