e1d027f99a355392a245cd800b4eb674e7ae43d8
Elwin <abc602456615@163.com>
2020-07-23 14:55:06 +0100

    Update enemy search

--- Starter_Pack/util.checkEnemy.js
+++ Starter_Pack/util.checkEnemy.js
@@ -3,9 +3,20 @@ var utilCheckEnemy = {
         for (let i in targets) {
             let roomInfo = Game.rooms[targets[i]];
             if (roomInfo) {
-                return targets[i];
+                if (roomInfo.find(FIND_HOSTILE_CREEPS).length === 0) {
+                    if (roomInfo.find(FIND_HOSTILE_STRUCTURES, {
+                        filter: function (structure) {
+                            return structure.structureType !== STRUCTURE_CONTROLLER;
+                        }
+                    }).length !== 0) {
+                        return targets[i];
+                    }
+                } else {
+                    return targets[i];
+                }
             }
         }
+        return targets[0];
     }
 };
 
