38a3fc627a13e2976643b0e7b3b5a6f558c7a686
Elwin <abc602456615@163.com>
2020-07-24 12:29:29 +0100

    def

--- Starter_Pack/init.def.js
+++ Starter_Pack/init.def.js
@@ -3,6 +3,9 @@
 var initDef = {
     run: function (creep) {
         let currentRoom = creep.room;
+        if (currentRoom.find(FIND_HOSTILE_CREEPS) >= 3) {
+            currentRoom.controller.activateSafeMode();
+        }
         var enemy = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS);
         if (enemy) {
             if (creep.attack(enemy) === ERR_NOT_IN_RANGE || creep.rangedAttack(enemy) === ERR_NOT_IN_RANGE) {
