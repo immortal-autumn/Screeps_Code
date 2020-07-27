f6759054229b4efbfda2d89521435e4183c4e973
Elwin <abc602456615@163.com>
2020-07-27 01:08:01 +0100

    update clear

--- Starter_Pack/util.clearCache.js
+++ Starter_Pack/util.clearCache.js
@@ -1,13 +1,14 @@
 var utilClearCache = {
     run: function (creep) {
         for (let name in Memory.creeps) {
-            if (Game.creeps[name].memory.belongs == null) {
-                delete Game.creeps[name].memory.belongs;
-                console.log('Rejudging belongings!')
-            }
             if (!Game.creeps[name]) {
                 delete Memory.creeps[name];
                 console.log('Clearing non-existing creep memory:', name);
+            } else {
+                if (Game.creeps[name].memory.belongs == null) {
+                    delete Game.creeps[name].memory.belongs;
+                    console.log('Rejudging belongings!')
+                }
             }
         }
     }
