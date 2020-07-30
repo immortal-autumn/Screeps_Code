3a2c88e237332409e03b78adbf8b4297e3dcf944
Elwin <abc602456615@163.com>
2020-07-30 12:21:25 +0100

    1

--- Starter_Pack/init.carry.js
+++ Starter_Pack/init.carry.js
@@ -84,7 +84,7 @@ var initCarry = {
                         break;
                     }
                     default: {
-                        let container = creep.room.findClosestByPath(FIND_MY_STRUCTURES, {
+                        let container = creep.findClosestByPath(FIND_MY_STRUCTURES, {
                             filter: (structure) => {
                                 return structure.structureType === STRUCTURE_STORAGE;
                             }
